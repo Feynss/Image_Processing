6c07bd193f328a28b21a7a2fbc4352705fb1936a
Feynsss <michavasnn@gmail.com>
2025-10-11 15:20:31 +0300

    3 task (hotfix)

--- Binarization_Edges_3/3_Binarization_Edges.py
+++ Binarization_Edges_3/3_Binarization_Edges.py
@@ -29,16 +29,21 @@ plt.show()
 lines = cv2.HoughLines(canny, 1, np.pi / 180, 120)
 
 if lines is not None:
-        for i in range(0, len(lines)):
-            rho = lines[i][0][0]
-            theta = lines[i][0][1]
-            a = math.cos(theta)
-            b = math.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
-            pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
-            cv2.line(image, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
+    linee = None
+    max_length = 0
+    for rho, theta in lines[:, 0]:
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a * rho
+        y0 = b * rho
+        pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
+        pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
+        length = np.sqrt((pt1[0] - pt2[0])**2 + (pt1[1] - pt2[1])**2)
+        if length > max_length:
+            max_length = length
+            linee = (pt1, pt2)
+    if linee:
+        cv2.line(image, linee[0], linee[1], (0, 0, 255), 7, cv2.LINE_AA)
 
 plt.imshow(image)
 plt.show()
@@ -55,15 +60,18 @@ T  = 50
 bin_img[image_gray < T] = 0
 bin_img[image_gray >= T] = 255
 plt.imshow(bin_img, cmap="gray")
+plt.title("Точечный")
 plt.show()
 
 _,th2 = cv2.threshold(image_gray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 plt.imshow(th2, cmap="gray")
+plt.title("Отсу")
 plt.show()
 
 th3 = cv2.adaptiveThreshold(image_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
             cv2.THRESH_BINARY,71,21)
 plt.imshow(th3, cmap="gray")
+plt.title("Адаптивная фильтрация")
 plt.show()
 
 scale = 0.5
@@ -79,6 +87,7 @@ plt.show()
 
 grad = cv2.addWeighted(grad_x, 0.5, grad_y, 0.5,0.0)
 plt.imshow((grad - grad.min())*255, cmap="gray")
+plt.title("Собеля")
 plt.show()
 
 edges = cv2.Canny(image_gray,100,200)
